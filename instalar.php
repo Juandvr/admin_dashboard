668fc87c167dc47f9dd381baa2053a3a42c93aac
Juandvr <villabonajuan203@gmail.com>
2024-05-29 18:23:20 -0500

    Crear instalar.php

--- instalar.php
+++ instalar.php
@@ -4,7 +4,7 @@ try {
 $conexion = new PDO('mysql:host=' . $config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['options']);
 $sql = file_get_contents("data/migracion.sql");
 $conexion->exec($sql);
-echo "La base de datos y la tabla de alumnos se han creado con éxito.";
+echo "La base de datos se ha creado con éxito.";
 } catch(PDOException $error) {
 echo $error->getMessage();
 }
