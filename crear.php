d749cd381c962a87ea45224102b1759f31e494cf
Juandvr <137643723+Juandvr@users.noreply.github.com>
2024-06-06 13:26:09 -0500

    Cambios de Natalia

--- crear.php
+++ crear.php
@@ -1,101 +1,118 @@
 <?php
 include 'funciones.php';
 if (isset($_POST['submit'])) {
-$resultado = [
-'error' => false,
-'mensaje' => 'La cita para ' . escapar($_POST['nombre_cliente']) . ' ha sido agregada con éxito'
-];
-$config = include 'config.php';
-try {
-$dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
-$conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
-$cita = array(
-"nombre_cliente" => $_POST['nombre_cliente'],
-"apellido" => $_POST['apellido'],
-"email" => $_POST['email'],
-"telefono" => $_POST['telefono'],
-"nombre_mascota" => $_POST['nombre_mascota'],
-"raza" => $_POST['raza'],
-"tamano" => $_POST['tamano'],
-"sexo" => $_POST['sexo'],
-"fecha" => $_POST['fecha'],
-);
-$consultaSQL = "INSERT INTO citas (nombre_cliente, apellido, email, telefono, nombre_mascota, raza, tamano, sexo, fecha) values (:" . implode(", :", array_keys($cita)) . ")";
-$sentencia = $conexion->prepare($consultaSQL);
-$sentencia->execute($cita);
-} catch(PDOException $error) {
-$resultado['error'] = true;
-$resultado['mensaje'] = $error->getMessage();
-}
+    $resultado = [
+        'error' => false,
+        'mensaje' => 'La cita para ' . escapar($_POST['nombre_cliente']) . ' ha sido agregada con éxito'
+    ];
+    $config = include 'config.php';
+    try {
+        $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
+        $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
+        $cita = array(
+            "nombre_cliente" => $_POST['nombre_cliente'],
+            "apellido" => $_POST['apellido'],
+            "email" => $_POST['email'],
+            "telefono" => $_POST['telefono'],
+            "nombre_mascota" => $_POST['nombre_mascota'],
+            "raza" => $_POST['raza'],
+            "tamano" => $_POST['tamano'],
+            "sexo" => $_POST['sexo'],
+            "fecha" => $_POST['fecha'],
+        );
+        $consultaSQL = "INSERT INTO citas (nombre_cliente, apellido, email, telefono, nombre_mascota, raza, tamano, sexo, fecha) values (:" . implode(", :", array_keys($cita)) . ")";
+        $sentencia = $conexion->prepare($consultaSQL);
+        $sentencia->execute($cita);
+    } catch (PDOException $error) {
+        $resultado['error'] = true;
+        $resultado['mensaje'] = $error->getMessage();
+    }
 }
 ?>
 
 <?php include "template/header.php"; ?>
 
-            <?php
-            if (isset($resultado)) {
-            ?>
-            <div class="container mt-3">
-            <div class="row">
+<?php
+if (isset($resultado)) {
+?>
+    <div class="container mt-3">
+        <div class="row">
             <div class="col-md-12">
-            <div class="alert alert-<?= $resultado['error'] ? 'danger' : 'success' ?>" role="alert">
-            <?= $resultado['mensaje'] ?>
+                <div class="alert alert-<?= $resultado['error'] ? 'danger' : 'success' ?>" role="alert">
+                    <?= $resultado['mensaje'] ?>
+                </div>
             </div>
-            </div>
-            </div>
-            </div>
-            <?php
-            }
-            ?>
-            <div class="container">
-            <div class="row">
-            <div class="col-md-12">
+        </div>
+    </div>
+<?php
+}
+?>
+<div class="container" id="formucita">
+    <div class="row">
+        <div class="col-md-12">
             <h2 class="mt-4">Crea una cita</h2>
             <hr>
             <form method="post">
-            <div class="form-group">
-            <label for="nombre_cliente">Nombre cliente</label>
-            <input type="text" name="nombre_cliente" id="nombre_cliente" class="form-control">
-            </div>
-            <div class="form-group">
-            <label for="apellido">Apellido</label>
-            <input type="text" name="apellido" id="apellido" class="form-control">
-            </div>
-            <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" class="form-control">
-            </div>
-            <div class="form-group">
-            <label for="telefono">Telefono</label>
-            <input type="text" name="telefono" id="telefono" class="form-control">
-            </div>
-            <div class="form-group">
-            <label for="nombre_mascota">Nombre mascota</label>
-            <input type="text" name="nombre_mascota" id="nombre_mascota" class="form-control">
-            </div>
-            <div class="form-group">
-            <label for="raza">Raza</label>
-            <input type="text" name="raza" id="raza" class="form-control">
-            </div>
-            <div class="form-group">
-            <label for="tamano">Tamaño</label>
-            <input type="text" name="tamano" id="tamano" class="form-control">
-            </div>
-            <div class="form-group">
-            <label for="sexo">Sexo</label>
-            <input type="text" name="sexo" id="sexo" class="form-control">
-            </div>
-            <div class="form-group">
-            <label for="fecha">Fecha</label>
-            <input type="date" name="fecha" id="fecha" class="form-control">
-            </div>
-            <div class="form-group">
-            <input type="submit" name="submit" class="btn btn-primary" value="Enviar">
-            <a class="btn btn-primary" href="index.php">Regresar al inicio</a>
-            </div>
+                <div class="form-group">
+                    <label for="nombre_cliente">Nombre cliente</label>
+                    <input type="text" name="nombre_cliente" id="nombre_cliente" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="apellido">Apellido</label>
+                    <input type="text" name="apellido" id="apellido" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="telefono">Telefono</label>
+                    <input type="text" name="telefono" id="telefono" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="nombre_mascota">Nombre mascota</label>
+                    <input type="text" name="nombre_mascota" id="nombre_mascota" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="raza">Raza</label>
+                    <input type="text" name="raza" id="raza" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="tamano">Tamaño</label>
+                    <input type="text" name="tamano" id="tamano" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="sexo">Sexo</label>
+                    <input type="text" name="sexo" id="sexo" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="fecha">Fecha</label>
+                    <input type="date" name="fecha" id="fecha" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="hora">Hora</label>
+                    <input type="time" name="hora" id="hora" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+                    <input type="submit" name="submit" class="btn btn-primary" value="Enviar"> 
+                </div>
+                <br>
+                <div class="form-group" id="regresar">
+                    <a class="btn btn-primary" href="index.php">Regresar al inicio</a>
+                </div>
             </form>
-            </div>
-            </div>
-            </div>
+        </div>
+    </div>
+</div>
 
-<?php include "templates/footer.php"; ?>
+<?php include "template/footer.php"; ?>
--- index.php
+++ index.php
@@ -3,18 +3,18 @@ include 'funciones.php';
 $error = false;
 $config = include 'config.php';
 try {
-$dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
-$conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
-if (isset($_POST['apellido'])) {
-$consultaSQL = "SELECT * FROM citas WHERE apellido LIKE '%" . $_POST['apellido'] . "%'";
-} else {
-$consultaSQL = "SELECT * FROM citas";
-}
-$sentencia = $conexion->prepare($consultaSQL);
-$sentencia->execute();
-$citas = $sentencia->fetchAll();
-} catch(PDOException $error) {
-$error= $error->getMessage();
+    $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
+    $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
+    if (isset($_POST['apellido'])) {
+        $consultaSQL = "SELECT * FROM citas WHERE apellido LIKE '%" . $_POST['apellido'] . "%'";
+    } else {
+        $consultaSQL = "SELECT * FROM citas";
+    }
+    $sentencia = $conexion->prepare($consultaSQL);
+    $sentencia->execute();
+    $citas = $sentencia->fetchAll();
+} catch (PDOException $error) {
+    $error = $error->getMessage();
 }
 $titulo = isset($_POST['apellido']) ? 'Lista de citas (' . $_POST['apellido'] . ')' : 'Lista de citas';
 ?>
@@ -22,80 +22,82 @@ $titulo = isset($_POST['apellido']) ? 'Lista de citas (' . $_POST['apellido'] . 
 <?php
 if ($error) {
 ?>
-<div class="container mt-2">
-<div class="row">
-<div class="col-md-12">
-<div class="alert alert-danger" role="alert">
-<?= $error ?>
-</div>
-</div>
-</div>
-</div>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-danger" role="alert">
+                    <?= $error ?>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
 }
 ?>
 <div class="">
-<div class="row">
-<div class="col-md-12">
-<a href="crear.php" class="btn btn-primary mt-4">Crear alumno</a>
-<hr>
-<form method="post" class="form-inline">
-<div class="form-group mr-3">
-<input type="text" id="apellido" name="apellido" placeholder="Buscar por apellido" class="form-control">
-</div>
-<button type="submit" name="submit" class="btn btn-primary">Ver resultados</button>
-</form>
-</div>
-</div>
+    <div class="row">
+        <div class="col-md-12">
+            <a href="crear.php" class="btn btn-primary mt-4">CREAR URUARIO</a>
+            <hr>
+            <form method="post" class="form-inline">
+                <div class="form-group mr-3">
+                    <input type="text" id="apellido" name="apellido" placeholder="Buscar por apellido" class="form-control">
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary">Ver resultados</button>
+            </form>
+        </div>
+    </div>
 </div>
 <div class="container">
-<div class="row">
-<div class="col-md-12">
-<h2 class="mt-3"><?= $titulo ?></h2>
-<table class="table">
-<thead>
-<tr>
-<th>#</th>
-<th>Nombre cliente</th>
-<th>Apellido</th>
-<th>Email</th>
-<th>Telefono</th>
-<th>Nombre mascota</th>
-<th>Raza</th>
-<th>Tamaño</th>
-<th>Sexo</th>
-<th>Fecha</th>
-<th>Acciones</th>
-</tr>
-</thead>
-<tbody>
-<?php
-if ($citas && $sentencia->rowCount() > 0) {
-foreach ($citas as $fila) {
-?>
-<tr>
-<td><?php echo escapar($fila["id"]); ?></td>
-<td><?php echo escapar($fila["nombre_cliente"]); ?></td>
-<td><?php echo escapar($fila["apellido"]); ?></td>
-<td><?php echo escapar($fila["email"]); ?></td>
-<td><?php echo escapar($fila["telefono"]); ?></td>
-<td><?php echo escapar($fila["nombre_mascota"]); ?></td>
-<td><?php echo escapar($fila["raza"]); ?></td>
-<td><?php echo escapar($fila["tamano"]); ?></td>
-<td><?php echo escapar($fila["sexo"]); ?></td>
-<td><?php echo escapar($fila["fecha"]); ?></td>
-<td>
-<a href="<?= 'borrar.php?id=' . escapar($fila["id"]) ?>"> Borrar</a>
-<a href="<?= 'editar.php?id=' . escapar($fila["id"]) ?>" . > Editar</a>
-</td>
-</tr>
-<?php
-}
-}
-?>
-<tbody>
-</table>
-</div>
-</div>
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="mt-3"><?= $titulo ?></h2>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre cliente</th>
+                        <th>Apellido</th>
+                        <th>Email</th>
+                        <th>Telefono</th>
+                        <th>Nombre mascota</th>
+                        <th>Raza</th>
+                        <th>Tamaño</th>
+                        <th>Sexo</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if ($citas && $sentencia->rowCount() > 0) {
+                        foreach ($citas as $fila) {
+                    ?>
+                            <tr>
+                                <td><?php echo escapar($fila["id"]); ?></td>
+                                <td><?php echo escapar($fila["nombre_cliente"]); ?></td>
+                                <td><?php echo escapar($fila["apellido"]); ?></td>
+                                <td><?php echo escapar($fila["email"]); ?></td>
+                                <td><?php echo escapar($fila["telefono"]); ?></td>
+                                <td><?php echo escapar($fila["nombre_mascota"]); ?></td>
+                                <td><?php echo escapar($fila["raza"]); ?></td>
+                                <td><?php echo escapar($fila["tamano"]); ?></td>
+                                <td><?php echo escapar($fila["sexo"]); ?></td>
+                                <td><?php echo escapar($fila["fecha"]); ?></td>
+                                <td><?php echo escapar($fila["hora"]); ?></td>
+                                <td>
+                                    <a href="<?= 'borrar.php?id=' . escapar($fila["id"]) ?>">🗑️Borrar</a>
+                                    <a href="<?= 'editar.php?id=' . escapar($fila["id"]) ?>" .>✏️Editar</a>
+                                </td>
+                            </tr>
+                    <?php
+                        }
+                    }
+                    ?>
+                <tbody>
+            </table>
+        </div>
+    </div>
 </div>
 <?php include "template/footer.php"; ?>
